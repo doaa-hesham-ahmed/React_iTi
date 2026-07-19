2d1ea230ba44575de69438145ca1ee383509d46c
doaa-hesham-ahmed <doaahesham452@gmail.com>
2026-07-19 23:55:59 +0300

    task9

---
+++ task9/task1.js
@@ -0,0 +1,32 @@
+let player1='scissors';//variable can be (rock,paper,scissors) output change in order to input
+let player2='paper';//variable can be (rock,paper,scissors) output change in order to input
+
+if(player1===player2){
+    console.log("its a tie");
+}
+else if(player1==='rock' && player2==='paper'){
+    console.log("player2 is win");
+}
+
+else if(player1==='paper' && player2==='rock'){
+    console.log("player1 is win");
+}
+
+
+else if(player1==='rock' && player2==='scissors'){
+    console.log("player1 is win");
+}
+else if(player1==='scissors' && player2==='rock'){
+    console.log("player2 is win");
+}
+
+else if(player1==='paper' && player2==='scissors'){
+    console.log("player2 is win");
+}
+
+else if(player1==='scissors' && player2==='paper'){
+    console.log("player1 is win");
+}
+else{
+    console.log("invalid choice");
+}
